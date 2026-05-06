6b45279d1681bb791886a088cf465ac35ee3543d
V.H.Nam | apikey:917406166639a085ed6580842d321c43422fc746faba8b96dc56665ce2dfb7cf <vhnam18032002@gmail.com>
2026-05-06 09:33:38 +0700

    BIG UPDATE CSS TO SCSS

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { resolve } from 'path';
 export default defineConfig({
   // Base path for deployment
   base: './',
+  publicDir: 'src/assets/static',
   build: {
     rollupOptions: {
       input: {
